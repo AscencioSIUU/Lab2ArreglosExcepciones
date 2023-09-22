62bd5ac7e122b048608adec8988cfc67f1fdaebb
PeDro0210 <pedroruben0210@gmail.com>
2023-09-21 19:50:37 -0600

    UML Update

--- src/Utilities/FileReaders/FileReaderRecinto.java
+++ src/Utilities/FileReaders/FileReaderRecinto.java
@@ -6,7 +6,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-import BasicObjects.Evento;
 import BasicObjects.Recinto;
 import Utilities.Seachers.RecintoSearcher;
 
